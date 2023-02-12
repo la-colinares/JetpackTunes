8ee6b03f38b299df61e8e680d4e79f9773ce787c
LA Colinares <dev.lacolinares@gmail.com>
2023-02-12 14:28:56 +0800

    Initial commit

---
+++ app/src/main/java/com/lacolinares/jetpacktunes/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.lacolinares.jetpacktunes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val VampireBlack = Color(0xFF0A0A0A)
+val MetallicYellow = Color(0xFFFFCF07)
+val OldSilver = Color(0xFF838383)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
